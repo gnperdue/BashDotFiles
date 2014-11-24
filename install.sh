2e589773204e8ebd0fd2192c4fb15390da4b4fa5
Gabriel Perdue <gnperdue@me.com>
2014-11-24 10:39:15 -0600

    Check for links before files...

--- install.sh
+++ install.sh
@@ -6,26 +6,26 @@
 
 DAT=`date -u +%s`
 
-if [ -e $HOME/.bash_profile ]; then
-  echo "Found existing bash profile as a file..."
-  echo " Making bash profile bck file..."
-  mv -v $HOME/.bash_profile $HOME/.bash_profile_bck_$DAT
-fi
 if [ -L $HOME/.bash_profile ]; then
   echo "Found existing bash profile as a link..."
   echo " Removing bash_profile symbolic link..."
   rm -v $HOME/.bash_profile
 fi
-
-if [ -e $HOME/.bash_prof_local ]; then
-  echo "Found existing bash_prof_local as a file..."
-  echo " Making bash_prof_local bck..."
-  mv -v $HOME/.bash_prof_local $HOME/.bash_prof_local_bck_$DAT
+if [ -e $HOME/.bash_profile ]; then
+  echo "Found existing bash profile as a file..."
+  echo " Making bash profile bck file..."
+  mv -v $HOME/.bash_profile $HOME/.bash_profile_bck_$DAT
 fi
+
 if [ -L $HOME/.bash_prof_local ]; then
   echo "Found existing bash prof local as a link..."
   echo " Removing bash_prof_local symbolic link..."
   rm -v $HOME/.bash_prof_local
+fi
+if [ -e $HOME/.bash_prof_local ]; then
+  echo "Found existing bash_prof_local as a file..."
+  echo " Making bash_prof_local bck..."
+  mv -v $HOME/.bash_prof_local $HOME/.bash_prof_local_bck_$DAT
 fi
 
 echo "Linking files..."
