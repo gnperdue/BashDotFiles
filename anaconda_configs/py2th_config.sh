9ca6c9a695489b258bae25385b4faed4887f0789
Gabriel Perdue <gnperdue@me.com>
2017-04-05 08:26:32 -0500

    start to track my anaconda setups via scripts for quick creattion and teardown, etc.

---
+++ anaconda_configs/py2th_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+conda info --envs
+conda remove --name py2th --all
+conda create -q -y -n py2th python=2.7
+. activate py2th
+
+conda install -q -y -c conda-forge pymc3
+conda install -q -y -c conda-forge scikit-image
+conda install -q -y -c conda-forge scikit-learn
+conda install -q -y -c conda-forge keras
+conda install -q -y flake8
+conda install -q -y jedi
+conda install -q -y ipython
+conda install -q -y jupyter
+conda install -q -y pymysql
+conda install -q -y pandas-datareader
---
+++ anaconda_configs/pyverchecker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+import importlib
+
+print('Python version: {}'.format(sys.version))
+
+
+def test_module_version(module_name):
+    try:
+        mod = importlib.import_module(module_name)
+        print('{} version: {}'.format(module_name, mod.__version__))
+    except ImportError:
+        print('Missing {}'.format(module_name))
+    except Exception as e:
+        print(e)
+
+modules = [
+    'pandas', 'matplotlib', 'numpy', 'scipy', 'IPython', 'sklearn',
+    'protobuf', 'numexpr', 'sympy', 'tensorflow', 'pymc3', 'theano',
+    'carl', 'h5py', 'keras'
+]
+
+for mod in modules:
+    test_module_version(mod)
+
+print("Note, Keras configuration is in $HOME/.keras/keras.json")
