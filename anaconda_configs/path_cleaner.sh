b6e9e1701f9b2e738e92e80e181896cbd08ffa23
Gabriel Perdue <perdue@fnal.gov>
2017-04-10 15:08:05 -0500

    py3 for nlp

---
+++ anaconda_configs/path_cleaner.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="${PATH/\/Users\/perdue\/anaconda\/bin:/}"
+export PATH="${PATH/\/Users\/perdue\/miniconda2\/bin:/}"
+export PATH="${PATH/\/Users\/perdue\/miniconda3\/bin:/}"
