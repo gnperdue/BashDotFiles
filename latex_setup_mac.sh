e6599cf14f2bf9e65edb2e80854f2683e4c73d1d
Gabriel Perdue <perdue@fnal.gov>
2017-04-24 14:09:13 -0500

    set up LaTeX preambles on a Mac

---
+++ latex_setup_mac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s $HOME/Dropbox/LaTeX/Preambles/mycodeformattingcpp.tex $HOME/Documents/mycodeformattingcpp.tex
+ln -s $HOME/Dropbox/LaTeX/Preambles/mycodeformattingr.tex $HOME/Documents/mycodeformattingr.tex
+ln -s $HOME/Dropbox/LaTeX/Preambles/mylatexproblemcounters.tex $HOME/Documents/mylatexproblemcounters.tex
+ln -s $HOME/Dropbox/LaTeX/Preambles/mypreamble.tex $HOME/Documents/mypreamble.tex
