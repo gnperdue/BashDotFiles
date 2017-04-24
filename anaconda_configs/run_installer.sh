ee1e5e56400b2c2ee35ee9a503160a8398d22ae6
Gabriel Perdue <perdue@fnal.gov>
2017-04-24 14:46:01 -0500

    run the installer

--- anaconda_configs/run_installer.sh
+++ anaconda_configs/run_installer.sh
@@ -4,4 +4,4 @@ SCRIPT=$1
 LOGFILE=`echo -e $SCRIPT | perl -ne 'chomp $_;@l=split("\\\.",$_);print @l[0].".log";'`
 echo "Logging install to $LOGFILE"
 echo "bash $SCRIPT | tee $LOGFILE"
-# bash $SCRIPT | tee $LOGFILE
+bash $SCRIPT | tee $LOGFILE
