2f132ab07970ad4ae7c32cf423371e46cbd3252c
Gabriel Perdue <perdue@fnal.gov>
2017-05-31 14:11:16 -0500

    put hostname in the log file

--- anaconda_configs/run_installer.sh
+++ anaconda_configs/run_installer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SCRIPT=$1
-LOGFILE=`echo -e $SCRIPT | perl -ne 'chomp $_;@l=split("\\\.",$_);print @l[0].".log";'`
+LOGFILE=`echo -e $SCRIPT | perl -ne 'chomp $_;@l=split("\\\.",$_);print @l[0];'`
+LOGFILE="${LOGFILE}_`hostname`.log"
 echo "Logging install to $LOGFILE"
 echo "bash $SCRIPT | tee $LOGFILE"
 bash $SCRIPT | tee $LOGFILE
