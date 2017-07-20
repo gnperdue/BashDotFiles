c9594926e20189adaa19603cd93d5b4ace41f3b8
Gabriel Perdue <gnperdue@me.com>
2017-07-20 09:52:38 -0500

    create conda environments for bare-bones TF training and testing and another for more feature rich prediction and analysis (include some db packages and plotting and convenience stuff)

---
+++ anaconda_configs/miniconda_py2mnv_trn.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# TensorFlow package for MINERvA TRAINING
+
+ARCH=`uname`
+echo `date`
+
+CONDAENVNAME="py2mnv_trn"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda2-latest-MacOSX-x86_64.sh
+  MINIDIR="miniconda2"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=2.7
+. activate $CONDAENVNAME
+
+export TF_PYTHON_URL=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.2.1-py2-none-any.whl
+pip install --ignore-installed --upgrade $TF_PYTHON_URL
+pip install h5py
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+
+echo -e "\a"
