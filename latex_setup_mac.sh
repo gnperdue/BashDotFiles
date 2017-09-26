e315fb41186fe28e27cede5c4692dadbd3c97573
Gabriel Perdue <gnperdue@me.com>
2017-09-26 13:25:10 -0500

    inlude instructions to go get TexShop

--- latex_setup_mac.sh
+++ latex_setup_mac.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# get TexShop
+# download zip file from http://pages.uoregon.edu/koch/texshop/obtaining.html
+# may or may not need to unzip - drag TeXShop to Applications folder
+
 ln -s $HOME/Dropbox/LaTeX/Preambles/mycodeformattingcpp.tex $HOME/Documents/mycodeformattingcpp.tex
 ln -s $HOME/Dropbox/LaTeX/Preambles/mycodeformattingr.tex $HOME/Documents/mycodeformattingr.tex
 ln -s $HOME/Dropbox/LaTeX/Preambles/mylatexproblemcounters.tex $HOME/Documents/mylatexproblemcounters.tex
