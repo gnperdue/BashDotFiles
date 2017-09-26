5ffe5766d11ee7d70191dc9f8bb8af2e5c34fa02
Gabriel Perdue <gnperdue@me.com>
2017-09-26 13:45:15 -0500

    more latex instructions

--- latex_setup_mac.sh
+++ latex_setup_mac.sh
@@ -3,6 +3,8 @@
 # get TexShop
 # download zip file from http://pages.uoregon.edu/koch/texshop/obtaining.html
 # may or may not need to unzip - drag TeXShop to Applications folder
+# get mactex
+# go to http://tug.org/mactex and download, install
 
 ln -s $HOME/Dropbox/LaTeX/Preambles/mycodeformattingcpp.tex $HOME/Documents/mycodeformattingcpp.tex
 ln -s $HOME/Dropbox/LaTeX/Preambles/mycodeformattingr.tex $HOME/Documents/mycodeformattingr.tex
