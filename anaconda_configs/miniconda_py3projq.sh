9fce193dec81c065b5d04c5b5474dc9c8441f9b4
Gabriel Perdue <gnperdue@me.com>
2017-10-02 18:11:29 -0500

    projectq in py3 with slow quantum simulator

---
+++ anaconda_configs/miniconda_py3projq.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+ARCH=`uname`
+
+CONDAENVNAME="py3projq"
+PYVER=3.5
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda3-latest-MacOSX-x86_64.sh
+  MINIDIR="miniconda3"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+conda install -q -y -c conda-forge scikit-image
+conda install -q -y -c conda-forge scikit-learn
+conda install -q -y flake8
+conda install -q -y jedi
+conda install -q -y ipython
+conda install -q -y jupyter
+conda install -q -y pymysql
+conda install -q -y pyyaml
+conda install -q -y pandas-datareader
+
+python -m pip install --user --global-option=--without-cppsimulator projectq
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+
+echo -e "\a"
