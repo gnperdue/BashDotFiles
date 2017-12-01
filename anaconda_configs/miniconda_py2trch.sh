f37ae2599779d6c52852cc3012d33b87cea7af3b
Gabriel Perdue <gnperdue@me.com>
2017-11-30 18:04:35 -0600

    script to install PyTorch for Python2

---
+++ anaconda_configs/miniconda_py2trch.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# PyTorch
+
+ARCH=`uname`
+echo `date`
+
+CONDAENVNAME="py2trch"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda2-latest-MacOSX-x86_64.sh
+  MINIDIR="miniconda2"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=2.7
+. activate $CONDAENVNAME
+
+conda install -q -y -c soumith pytorch torchvision
+conda install -q -y -c conda-forge scikit-image
+conda install -q -y -c conda-forge scikit-learn
+conda install -q -y flake8
+conda install -q -y jedi
+conda install -q -y ipython
+conda install -q -y jupyter
+conda install -q -y pymysql
+conda install -q -y sqlalchemy
+conda install -q -y pandas-datareader
+conda install -q -y h5py
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+
+echo -e "\a"
