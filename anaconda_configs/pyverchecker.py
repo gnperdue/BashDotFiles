ccc6560c989ba0f4f8544a48e5114b21293b8bc2
Gabriel Perdue <gnperdue@me.com>
2017-12-05 17:38:48 -0600

    check for Horovod

--- anaconda_configs/pyverchecker.py
+++ anaconda_configs/pyverchecker.py
@@ -23,7 +23,7 @@ modules = [
     'pandas', 'matplotlib', 'numpy', 'scipy', 'IPython', 'sklearn',
     'skimage', 'protobuf', 'numexpr', 'sympy', 'tensorflow', 'pymc3',
     'theano', 'h5py', 'keras', 'xlrd', 'lasagne', 'yaml', 'nltk',
-    'deepdish', 'torch', 'tornado', 'gevent'
+    'deepdish', 'torch', 'tornado', 'gevent', 'horovod'
 ]
 
 for mod in modules:
