5d3c5b152900a723362bdc2e32b94913e4c9b54d
Gabriel Perdue <gnperdue@me.com>
2018-01-28 06:42:48 -0600

    py3 with TF 1.5

---
+++ anaconda_configs/miniconda_py3tf15.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# TensorFlow 1.5 and some useful parts of the scipy universe
+
+ARCH=`uname`
+echo `date`
+
+PYMAJOR=3
+PYMINOR=6
+CONDAENVNAME="py${PYMAJOR}tf15"
+PYVER="${PYMAJOR}.${PYMINOR}"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda${PYMAJOR}-latest-MacOSX-x86_64.sh
+  MINIDIR="miniconda${PYMAJOR}"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+conda install -q -y -c conda-forge scikit-image
+conda install -q -y -c conda-forge scikit-learn
+conda install -q -y flake8
+conda install -q -y jedi
+conda install -q -y ipython
+conda install -q -y jupyter
+conda install -q -y pymysql
+conda install -q -y sqlalchemy
+conda install -q -y pandas-datareader
+conda install -q -y pyqtgraph
+
+export TF_PYTHON_URL=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.5.0-py3-none-any.whl
+pip install --ignore-installed --upgrade $TF_PYTHON_URL
+pip install h5py
+pip install prettytensor
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+
+echo -e "\a"
