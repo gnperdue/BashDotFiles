ac69d26f372b8a3f57c398904fd899e9d28599cc
Gabriel Perdue <gnperdue@me.com>
2018-07-16 05:54:03 -0500

    basic TF for TensorBoard

--- anaconda_configs/miniconda_py3tf19.sh
+++ anaconda_configs/miniconda_py3tf19.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TensorFlow 1.8 and some useful parts of the scipy universe
+# TensorFlow 1.9 and h5py
 
 ARCH=`uname`
 echo `date`
@@ -8,7 +8,7 @@ echo `date`
 PYMAJOR=3
 PYMINOR=6
 TFMAJOR=1
-TFMINOR=8
+TFMINOR=9
 TFPATCH=0
 CONDAENVNAME="py${PYMAJOR}tf${TFMAJOR}${TFMINOR}"
 PYVER="${PYMAJOR}.${PYMINOR}"
@@ -36,22 +36,8 @@ conda remove --yes --name $CONDAENVNAME --all
 conda create -q -y -n $CONDAENVNAME python=$PYVER
 . activate $CONDAENVNAME
 
-conda install -q -y -c conda-forge scikit-image
-conda install -q -y -c conda-forge scikit-learn
-conda install -q -y flake8
-conda install -q -y jedi
-conda install -q -y ipython
-conda install -q -y jupyter
-conda install -q -y pymysql
-conda install -q -y sqlalchemy
-conda install -q -y pandas-datareader
-conda install -q -y pyqtgraph
-conda install -q -y xlrd
-conda install -q -y cython
-
 pip install --ignore-installed --upgrade $TF_PYTHON_URL
 pip install h5py
-# pip install prettytensor
 
 echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
 echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
