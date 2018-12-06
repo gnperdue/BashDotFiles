3089d51d3e2ee3680446022bed826a2ba0245bb6
Gabriel Perdue <gnperdue@gmail.com>
2018-12-06 14:59:01 -0600

    py3 astropy with tf

---
+++ anaconda_configs/miniconda_py3astro.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# "py3astro" - for Python 3 with astropy and some basic ML stuff.
+
+ARCH=`uname`
+echo `date`
+
+PYMAJOR=3
+PYMINOR=6
+TFMAJOR=1
+TFMINOR=12
+TFPATCH=0
+CONDAENVNAME="py${PYMAJOR}astro"
+PYVER="${PYMAJOR}.${PYMINOR}"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda${PYMAJOR}-latest-MacOSX-x86_64.sh
+  MINIDIR="miniconda${PYMAJOR}"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+pip install --upgrade pip
+pip install 'python-language-server[all]'
+
+conda install -q -y tensorflow=${TFMAJOR}.${TFMINOR}.${TFPATCH}
+conda install -q -y -c conda-forge xgboost
+conda install -q -y scikit-image
+conda install -q -y scikit-learn
+conda install -q -y ipython
+conda install -q -y jupyter
+conda install -q -y astropy
+conda install -q -y seaborn
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+
+echo -e "\a"
