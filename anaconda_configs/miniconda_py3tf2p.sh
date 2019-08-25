ba698de7e1fd84fff6e217f5905a162ed0058dc4
Gabriel Perdue <gnperdue@me.com>
2019-08-25 07:23:04 -0500

    add a tf2 nightly w/ tf prob env, modernize pyverchecker a bit

---
+++ anaconda_configs/miniconda_py3tf2p.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# "py3tf2p" - TF 2.0 Preview w/ TF probability
+
+ARCH=`uname`
+echo `date`
+
+PYMAJOR=3
+PYMINOR=6
+TFMAJOR=2
+TFMINOR=0
+TFPATCH=0
+CONDAENVNAME="py${PYMAJOR}tf${TFMAJOR}p"
+PYVER="${PYMAJOR}.${PYMINOR}"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda${PYMAJOR}-latest-MacOSX-x86_64.sh
+  MINIDIR="miniconda${PYMAJOR}"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install --no-cache-dir 'python-language-server[all]'
+
+conda install -q -y ipython
+conda install -q -y jupyter
+conda install -q -y tqdm
+conda install -q -y coverage
+
+pip install --no-cache-dir -q --upgrade tf-nightly-2.0-preview
+pip install --no-cache-dir -q --upgrade tensorflow-probability
+pip install --no-cache-dir matplotlib seaborn imageio \
+    scikit-image scikit-learn
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+echo "  (or possibly, for newer versions of Anaconda)"
+echo "\$ conda deactivate"
+
+echo -e "\a"
