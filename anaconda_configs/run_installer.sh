1a3d6a1cb93a34015ab4eab7388ae74701cc3793
Gabriel Perdue <gnperdue@gmail.com>
2019-09-12 15:38:33 -0500

    Update install log to include date in human-readable format

--- anaconda_configs/run_installer.sh
+++ anaconda_configs/run_installer.sh
@@ -2,7 +2,7 @@
 
 SCRIPT=$1
 LOGFILE=`echo -e $SCRIPT | perl -ne 'chomp $_;@l=split("\\\.",$_);print @l[0];'`
-LOGFILE="${LOGFILE}_`hostname`.log"
+LOGFILE="${LOGFILE}_`hostname`_`date +%Y-%m-%d`.log"
 echo "Logging install to $LOGFILE"
 echo "bash $SCRIPT | tee $LOGFILE"
 bash $SCRIPT | tee $LOGFILE
