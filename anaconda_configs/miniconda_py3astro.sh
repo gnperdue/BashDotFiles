4eac3b13156f6fd2d1f485d6cac1553a0c3878fc
Gabriel Perdue <gnperdue@gmail.com>
2019-11-26 14:48:04 -0600

    update astropy env

--- anaconda_configs/miniconda_py3astro.sh
+++ anaconda_configs/miniconda_py3astro.sh
@@ -7,9 +7,6 @@ echo `date`
 
 PYMAJOR=3
 PYMINOR=7
-TFMAJOR=1
-TFMINOR=14
-TFPATCH=0
 CONDAENVNAME="py${PYMAJOR}astro"
 PYVER="${PYMAJOR}.${PYMINOR}"
 CONDAINSTALL=""
@@ -38,7 +35,9 @@ conda create -q -y -n $CONDAENVNAME python=$PYVER
 pip install --upgrade pip
 pip install 'python-language-server[all]'
 
-conda install -q -y tensorflow=${TFMAJOR}.${TFMINOR}.${TFPATCH}
+conda install -q -y tensorflow  # should get 2.X by default now
+conda install -q -y -c pytorch -c fastai fastai
+conda install -q -y -c conda-forge tensorboardx
 conda install -q -y -c conda-forge xgboost
 conda install -q -y scikit-image
 conda install -q -y scikit-learn
@@ -47,6 +46,7 @@ conda install -q -y jupyter
 conda install -q -y astropy
 conda install -q -y seaborn
 conda install -q -y colorcet
+conda install -q -y coverage
 
 echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
 echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
@@ -55,6 +55,6 @@ echo "\$ source activate $CONDAENVNAME"
 echo "and deactivate with"
 echo "\$ source deactivate"
 
-python pyverchecker.py
+conda list
 
 echo -e "\a"
