21a18557d23672dc944cf6c897e9f6b769c290b3
Gabriel Perdue <gnperdue@me.com>
2020-05-28 14:18:47 -0500

    somewhat simpler set of packages, basically all via pip instead of conda

--- anaconda_configs/miniconda_py3qiskit.sh
+++ anaconda_configs/miniconda_py3qiskit.sh
@@ -39,25 +39,31 @@ pip install --upgrade pip
 pip install --upgrade setuptools
 pip install 'python-language-server[all]'
 
-conda install -q -y pytorch torchvision -c pytorch
-conda install -q -y -c conda-forge xgboost
-conda install -q -y scikit-image
-conda install -q -y scikit-learn
-conda install -q -y ipython
-conda install -q -y jupyter
-conda install -q -y pymysql
-conda install -q -y sqlalchemy
-conda install -q -y pandas-datareader
-conda install -q -y pyqtgraph
-conda install -q -y xlrd
-conda install -q -y cython
-conda install -q -y pyyaml
-conda install -q -y sympy
-conda install -q -y tqdm
-conda install -q -y poppler     # for qiskit circuit drawer
-conda install -q -y ipywidgets  # for qiskit viz
+# conda install -q -y pytorch torchvision -c pytorch
+# conda install -q -y -c conda-forge xgboost
+# conda install -q -y scikit-image
+# conda install -q -y scikit-learn
+# conda install -q -y ipython
+# conda install -q -y jupyter
+# conda install -q -y pymysql
+# conda install -q -y sqlalchemy
+# conda install -q -y pandas-datareader
+# conda install -q -y pyqtgraph
+# conda install -q -y xlrd
+# conda install -q -y cython
+# conda install -q -y pyyaml
+# conda install -q -y sympy
+# conda install -q -y tqdm
+# conda install -q -y poppler     # for qiskit circuit drawer
+# conda install -q -y ipywidgets  # for qiskit viz
 
-pip install qiskit qiskit-aqua qiskit-chemistry
+pip install qiskit
+pip install matplotlib
+pip install juypter
+pip install ipywidgets
+pip install seaborn
+pip install pygments
+pip install pyyaml
 
 echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
 echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
