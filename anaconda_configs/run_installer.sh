74d657daa3ddad360fd88c1942434f3bbb7922e5
Gabriel Perdue <gnperdue@hey.com>
2020-09-08 12:28:26 -0500

    log errors to the log file also

--- anaconda_configs/run_installer.sh
+++ anaconda_configs/run_installer.sh
@@ -5,5 +5,5 @@ LOGFILE=`echo -e $SCRIPT | perl -ne 'chomp $_;@l=split("\\\.",$_);print @l[0];'`
 LOGFILE="${LOGFILE}_`hostname`_`date +%Y-%m-%d`.log"
 echo "Logging install to $LOGFILE"
 echo "bash $SCRIPT | tee $LOGFILE"
-bash $SCRIPT | tee $LOGFILE
+bash $SCRIPT 2>&1 | tee $LOGFILE
 # python pyverchecker.py >> $LOGFILE
