bc9db4fcd56d03d58320545211e67e8abf0ed38c
Gabriel Perdue <gnperdue@hey.com>
2021-11-15 12:49:55 -0600

    Julia on the MBP2020

---
+++ install_mbp2020.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Check to see if the bash profiles already exist. If they do, archvie them
+# into _bck files unless they are just symbolic links, in which case just
+# remove the old links.
+
+DAT=`date -u +%s`
+
+filelist='zshrc gitignore_global'
+
+for flnm in $filelist
+do
+  if [ -L $HOME/.$flnm ]; then
+    echo "Found existing $flnm as a link..."
+    echo " Removing $flnm symbolic link..."
+    rm -v $HOME/.$flnm
+  fi
+  if [ -e $HOME/.$flnm ]; then
+    echo "Found existing $flnm as a file..."
+    echo " Making $flnm bck file..."
+    mv -v $HOME/.$flnm $HOME/.${flnm}_bck_${DAT}
+  fi
+done
+
+# uncomment this for Clojure
+#mkdir -p $HOME/.lein
+#cp lein/profiles.clj $HOME/.lein/profiles.clj
+
+# Julia
+sudo ln -s /Applications/Julia-1.6.app/Contents/Resources/julia/bin/julia /usr/local/bin/julia
+
+echo "Copying files..."
+for flnm in $filelist
+do
+  cp -v `pwd`/$flnm $HOME/.$flnm
+done
+echo " "
+
+# be careful with this since it will over-write a token you can't look at on
+# GitHub.
+#cp gitconfig ~/.gitconfig
+#echo "You need to update the token in your ~/.gitconfig"
+echo "Done."
