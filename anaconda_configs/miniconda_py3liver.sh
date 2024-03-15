1428d55bb3e535adfa49cc605ac4f122d0c4c486
Gabriel Perdue <gnperdue@hey.com>
2024-03-15 13:58:18 -0500

    liverpool collaboration error correction env

---
+++ anaconda_configs/miniconda_py3liver.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# "pyliver" - Liverpool+SQMS QEC work
+
+ARCH=`uname`
+echo `date`
+
+PYMAJOR=3
+PYMINOR=10
+CONDAENVNAME="py${PYMAJOR}liver"
+PYVER="${PYMAJOR}.${PYMINOR}"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda${PYMAJOR}-latest-MacOSX-x86_64.sh
+  MINIDIR="miniconda${PYMAJOR}"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+pip install --upgrade pip
+pip install --upgrade setuptools
+
+conda install -q -y ipython
+conda install -q -y jupyter
+conda install -q -y tqdm
+conda install -q -y coverage
+conda install -q -y numpy
+conda install -q -y matplotlib
+conda install -q -y scipy
+
+pip install --no-cache-dir qiskit
+pip install --no-cache-dir qiskit-aer
+pip install --no-cache-dir pylatexenc
+pip install --no-cache-dir pymatching
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+echo "  (or possibly, for newer versions of Anaconda)"
+echo "\$ conda deactivate"
+
+python pyverchecker.py
+
+echo -e "\a"
