e50bea50cb580adfab8d7f333cb0ef9ef94717fb
Gabriel Perdue <gnperdue@hey.com>
2024-04-05 13:53:34 -0500

    update to pip-based install

--- anaconda_configs/miniconda_py3cirq.sh
+++ anaconda_configs/miniconda_py3cirq.sh
@@ -10,7 +10,7 @@ ARCH=`uname`
 echo `date`
 
 PYMAJOR=3
-PYMINOR=7
+PYMINOR=10
 CONDAENVNAME="py${PYMAJOR}cirq"
 PYVER="${PYMAJOR}.${PYMINOR}"
 CONDAINSTALL=""
@@ -36,7 +36,8 @@ conda remove --yes --name $CONDAENVNAME --all
 conda create -q -y -n $CONDAENVNAME python=$PYVER
 . activate $CONDAENVNAME
 
-# pip install --upgrade pip
+pip install --upgrade pip
+pip install --upgrade setuptools
 # pip install 'python-language-server[all]'
 
 # pip install tensorflow==2.4.1
@@ -47,20 +48,53 @@ conda create -q -y -n $CONDAENVNAME python=$PYVER
 # pip install scikit-learn
 # pip install scikit-image
 
+# doesn't work...
+# REQNAME="requirements_${CONDAENVNAME}.txt"
+# rm -vf $REQNAME
+# cat <<EOT >> $REQNAME
+# cirq
+# ipython
+# jupyter<=1.0.0
+# matplotlib
+# numpy
+# scikit-learn
+# scipy
+# EOT
+# pip install -r $REQNAME
+
+# doesn't work
+# REQNAME="requirements_${CONDAENVNAME}.txt"
+# rm -vf $REQNAME
+# cat <<EOT >> $REQNAME
+# cirq
+# ipython
+# matplotlib
+# notebook
+# numpy
+# scikit-learn
+# scipy
+# EOT
+# pip install -r $REQNAME
+
+# doesn't work
+# REQNAME="requirements_${CONDAENVNAME}.txt"
+# rm -vf $REQNAME
+# cat <<EOT >> $REQNAME
+# cirq==1.3
+# ipython
+# jupyter<=1.0.0
+# matplotlib
+# numpy
+# EOT
+# pip install -r $REQNAME
+
 REQNAME="requirements_${CONDAENVNAME}.txt"
 rm -vf $REQNAME
 cat <<EOT >> $REQNAME
-cirq
-ipython
-jupyter<=1.0.0
-matplotlib
-numpy
-scikit-learn
-scipy
+cirq<=1.2
+jupyter
 EOT
 pip install -r $REQNAME
-
-
 
 echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
 echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
