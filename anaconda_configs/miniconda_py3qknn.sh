23631f73a267fe1f632595f6a31830676aae6f83
Gabriel Perdue <gnperdue@hey.com>
2024-10-01 14:56:19 -0500

    quantum work

---
+++ anaconda_configs/miniconda_py3qknn.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# "py3qknn"
+
+ARCH=`uname`
+echo `date`
+
+PYMAJOR=3
+PYMINOR=10
+CONDAENVNAME="py${PYMAJOR}qknn"
+PYVER="${PYMAJOR}.${PYMINOR}"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda${PYMAJOR}-latest-MacOSX-x86_64.sh
+  MINIDIR="miniconda${PYMAJOR}"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install -r requirements_qknn.txt
+pip install --upgrade coverage
+
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+echo "  (or possibly, for newer versions of Anaconda)"
+echo "\$ conda deactivate"
+
+#python pyverchecker.py
+
+echo -e "\a"
