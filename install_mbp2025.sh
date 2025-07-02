bd7cbecd4011a54a511b71eff48ee3db4c8e1c92
Gabriel Perdue <gnperdue@hey.com>
2025-07-02 16:11:26 -0500

    MBP 2025 specific files

---
+++ install_mbp2025.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Check to see if the bash profiles already exist. If they do, archvie them
+# into _bck files unless they are just symbolic links, in which case just
+# remove the old links.
+
+DAT=`date -u +%s`
+
+filelist='zshrc gitignore_global'
+
+for flnm in $filelist
+do
+  if [ -L $HOME/.$flnm ]; then
+    echo "Found existing $flnm as a link..."
+    echo " Removing $flnm symbolic link..."
+    rm -v $HOME/.$flnm
+  fi
+  if [ -e $HOME/.$flnm ]; then
+    echo "Found existing $flnm as a file..."
+    echo " Making $flnm bck file..."
+    mv -v $HOME/.$flnm $HOME/.${flnm}_bck_${DAT}
+  fi
+done
+
+echo "Copying files..."
+for flnm in $filelist
+do
+  cp -v `pwd`/$flnm $HOME/.$flnm
+done
+cp -v `pwd`/zshrc_mbp2025 $HOME/.zshrc_local
+echo " "
+
+# be careful with this since it will over-write a token you can't look at on
+# GitHub.
+#cp gitconfig ~/.gitconfig
+#echo "You need to update the token in your ~/.gitconfig"
+echo "Done."
