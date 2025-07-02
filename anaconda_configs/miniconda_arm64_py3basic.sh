6664b7a683ebe0e91bb6ca9ac3d390478601e41a
Gabriel Perdue <gnperdue@hey.com>
2025-07-02 16:13:17 -0500

    super simple Python3

---
+++ anaconda_configs/miniconda_arm64_py3basic.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# "py3basic" - Simple Python 3.
+
+ARCH=`uname`
+CHIP=`arch`
+echo `date`
+CONDAENVNAMEEXT="basic"
+
+PYMAJOR=3
+PYMINOR=13
+CONDAENVNAME="py${PYMAJOR}${CONDAENVNAMEEXT}"
+PYVER="${PYMAJOR}.${PYMINOR}"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda${PYMAJOR}-latest-MacOSX-${CHIP}.sh
+  MINIDIR="miniconda${PYMAJOR}"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+pip install --upgrade pip
+
+conda install -q -y ipython
+conda install -q -y jupyter
+
+  
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+echo "  (or possibly, for newer versions of Anaconda)"
+echo "\$ conda deactivate"
+
+python pyverchecker.py
+
+echo -e "\a"
