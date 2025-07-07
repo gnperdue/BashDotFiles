2462f98881cb6d2933146f0b81b63fd016a68538
Gabriel Perdue <gnperdue@hey.com>
2025-07-07 13:59:19 -0500

    feat(anaconda_configs): add setup script for ARM64 Python 3.13 environment
    Introduced a new setup script, miniconda_arm64_py3stim.sh, to configure a Conda environment tailored for Python 3.13 on ARM64 architecture, specifically supporting MacOS only. Adjusted zshrc_mbp2025 to include an alias for activating this environment. This change facilitates the installation and management of Python packages necessary for scientific computation using Stim on compatible Mac systems.

---
+++ anaconda_configs/miniconda_arm64_py3stim.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+ARCH=`uname`
+CHIP=`arch`
+echo `date`
+PYMAJOR=3
+PYMINOR=13
+PYVER="${PYMAJOR}.${PYMINOR}"
+
+CONDAENVNAME="py${PYMAJOR}stim"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda${PYMAJOR}-latest-MacOSX-${CHIP}.sh
+  MINIDIR="miniconda${PYMAJOR}"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install -r requirements_arm64_stim.txt
+
+
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+echo "  (or possibly, for newer versions of Anaconda)"
+echo "\$ conda deactivate"
+
+#python pyverchecker.py
+
+echo -e "\a"
