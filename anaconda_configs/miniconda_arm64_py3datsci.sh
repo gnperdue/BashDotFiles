6c8147eb0a8247d12d371984b66458ce5c66e162
Gabriel Perdue <gnperdue@hey.com>
2025-07-24 10:30:14 -0500

    feat(anaconda): add Miniconda configuration script for ARM64 data science setup
    This commit introduces a new shell script, `miniconda_arm64_py3datsci.sh`, for setting up a Python 3.13 environment with data science libraries on ARM64 Mac OSX. The script automates the installation of Miniconda, creates a Conda environment named "py3datsci," and installs essential packages such as numpy, scipy, pandas, and scikit-learn. It also provides usage instructions for activating and deactivating the environment. Currently, it is tailored specifically for Mac OSX systems.

---
+++ anaconda_configs/miniconda_arm64_py3datsci.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# "py3basic" - Simple Python 3.
+
+ARCH=`uname`
+CHIP=`arch`
+echo `date`
+CONDAENVNAMEEXT="datsci"
+
+PYMAJOR=3
+PYMINOR=13
+CONDAENVNAME="py${PYMAJOR}${CONDAENVNAMEEXT}"
+PYVER="${PYMAJOR}.${PYMINOR}"
+CONDAINSTALL=""
+MINIDIR=""
+
+if [[ $ARCH == "Darwin" ]]; then
+  CONDAINSTALL=Miniconda${PYMAJOR}-latest-MacOSX-${CHIP}.sh
+  MINIDIR="miniconda${PYMAJOR}"
+else
+  echo "Only configured for Mac OSX so far."
+  exit 1
+fi
+
+if [[ ! -d "$HOME/$MINIDIR" ]]; then
+  wget https://repo.continuum.io/miniconda/$CONDAINSTALL
+  bash $CONDAINSTALL -b -p $HOME/$MINIDIR
+fi
+
+export PATH="$HOME/$MINIDIR/bin:$PATH"
+
+conda info --envs
+conda remove --yes --name $CONDAENVNAME --all
+conda create -q -y -n $CONDAENVNAME python=$PYVER
+. activate $CONDAENVNAME
+
+pip install --upgrade pip
+
+conda install -q -y ipython notebook
+conda install -q -y scipy numpy matplotlib pandas scikit-learn
+conda install -q -y tqdm coverage
+
+  
+echo "Be sure to adjust your PATH and include $HOME/$MINIDIR/bin"
+echo " e.g., export PATH=$HOME/$MINIDIR/bin:\$PATH"
+echo "Activate the $CONDAENVNAME environment with"
+echo "\$ source activate $CONDAENVNAME"
+echo "and deactivate with"
+echo "\$ source deactivate"
+echo "  (or possibly, for newer versions of Anaconda)"
+echo "\$ conda deactivate"
+
+python pyverchecker.py
+
+echo -e "\a"
